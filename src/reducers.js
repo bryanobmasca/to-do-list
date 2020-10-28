7cbd4ac5259a14154ce5cefab2b1a874412a492c
John Emmanuel Bacalla <john.emmanuel.bacalla@oocl.com>
2020-10-28 15:38:53 +0800

    [TodoList][Bryan-John] refactor: Use switch-case in reducer

--- src/reducers.js
+++ src/reducers.js
@@ -1,20 +1,21 @@
 import { combineReducers } from "redux";
 
 const todos = (state = [], action) => {
-    if (action.type === "ADD_TO_DO") {
-        return [...state, action.payload];
+    switch (action.type) {
+        case "ADD_TO_DO":
+            return [...state, action.payload];
+        case "REVERT_STATUS":
+            return state.map(todo =>
+                todo.id === action.payload ? { ...todo, done: !todo.done }
+                    : todo);
+        case "DELETE_TO_DO":
+            const todos = state.filter(todo => todo.id !== action.payload);
+            return todos;
+        case "INIT_TODO":
+            return action.payload;
+        default:
+            return state;
     }
-    else if (action.type === "REVERT_STATUS") {
-        return state.map(todo =>
-            todo.id === action.payload ? { ...todo, done: !todo.done }
-                : todo);
-    } else if (action.type === "DELETE_TO_DO") {
-        const todos = state.filter(todo => todo.id !== action.payload);
-        return todos;
-    } else if (action.type === "INIT_TODO") {
-        return action.payload
-    }
-    return state;
 }
 
 export default combineReducers({
