418e7182257eb773d980e3ee6fa79902f1490695
John Emmanuel Bacalla <john.emmanuel.bacalla@oocl.com>
2020-10-28 15:54:36 +0800

    [TodoList][Bryan-John] refactor: Change actions to one-liner

--- src/actions.js
+++ src/actions.js
@@ -3,18 +3,7 @@ export const TOGGLE_TODO = "TOGGLE_TODO";
 export const DELETE_TO_DO = "DELETE_TO_DO";
 export const INIT_TODO = "INIT_TODO";
 
-export const addToDo = (todo) => {
-    return { type: ADD_TO_DO, payload: todo };
-}
-
-export const toggleTodo = (id) => {
-    return { type: TOGGLE_TODO, payload: id }
-}
-
-export const deleteToDo = (id) => {
-    return { type: DELETE_TO_DO, payload: id }
-}
-
-export const initTodos = todos => {
-    return { type: INIT_TODO, payload: todos }
-}
+export const addToDo = todo => ({ type: ADD_TO_DO, payload: todo });
+export const toggleTodo = id => ({ type: TOGGLE_TODO, payload: id });
+export const deleteToDo = id => ({ type: DELETE_TO_DO, payload: id });
+export const initTodos = todos => ({ type: INIT_TODO, payload: todos });
