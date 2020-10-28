be6d767513823ed28490c10a39d350b571611894
John Emmanuel Bacalla <john.emmanuel.bacalla@oocl.com>
2020-10-28 15:36:02 +0800

    [TodoList][Bryan-John] refactor: Add required property in input text

--- src/Components/ToDoGenerator/index.js
+++ src/Components/ToDoGenerator/index.js
@@ -7,10 +7,7 @@ class ToDoGenerator extends Component {
     onSubmit = (event) => {
         event.preventDefault();
         const text = event.target.toDoText.value;
-        if (text === "" || text === null) {
-            alert("To-Do item must be filled out");
-            return false;
-        }
+        
         addTodo(text).then(response => {
             this.props.addToDo(response.data);
             event.target.toDoText.value = "";
@@ -20,7 +17,7 @@ class ToDoGenerator extends Component {
     render() {
         return (
             <form onSubmit={this.onSubmit}>
-                <input type="text" name="toDoText" id="toDoText" placeholder="Input new to-do here..." />
+                <input type="text" name="toDoText" id="toDoText" placeholder="Input new to-do here..." required />
                 <input type="submit" value="Add" />
             </form>
         );
