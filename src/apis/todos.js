3f22219144ee1b6886e922767ee173f85b881946
John Emmanuel Bacalla <john.emmanuel.bacalla@oocl.com>
2020-10-28 16:00:13 +0800

    [TodoList][Bryan-John] refactor: Change todos to one-liner

--- src/apis/todos.js
+++ src/apis/todos.js
@@ -1,17 +1,6 @@
 import api from './api'
 
-export const getTodos = () => {
-    return api.get('/api/todos')
-}
-
-export const addTodo = (text) => {
-    return api.post('/api/todos', {text})
-}
-
-export const deleteTodo = (id) => {
-    return api.delete(`/api/todos/${id}`)
-}
-
-export const toggleTodo = ({id, done}) => {
-    return api.put(`/api/todos/${id}`, {done: !done})
-}
+export const getTodos = () => (api.get('/api/todos'))
+export const addTodo = (text) => (api.post('/api/todos', { text }))
+export const deleteTodo = (id) => (api.delete(`/api/todos/${id}`))
+export const toggleTodo = ({ id, done }) => (api.put(`/api/todos/${id}`, { done: !done }))
