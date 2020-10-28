49b7bd33daa45473a2f417d8d80a2d0cc10632c4
John Emmanuel Bacalla <john.emmanuel.bacalla@oocl.com>
2020-10-28 15:42:22 +0800

    [TodoList][Bryan-John] refactor: Extract action types to constant

--- src/actions.js
+++ src/actions.js
@@ -1,15 +1,20 @@
+export const ADD_TO_DO = "ADD_TO_DO";
+export const REVERT_STATUS = "REVERT_STATUS";
+export const DELETE_TO_DO = "DELETE_TO_DO";
+export const INIT_TODO = "INIT_TODO";
+
 export const addToDo = (todo) => {
-    return { type: "ADD_TO_DO", payload: todo };
+    return { type: ADD_TO_DO, payload: todo };
 }
 
 export const revertStatus = (id) => {
-    return { type: "REVERT_STATUS", payload: id }
+    return { type: REVERT_STATUS, payload: id }
 }
 
 export const deleteToDo = (id) => {
-    return { type: "DELETE_TO_DO", payload: id }
+    return { type: DELETE_TO_DO, payload: id }
 }
 
 export const initTodos = todos => {
-    return { type: "INIT_TODO", payload: todos }
+    return { type: INIT_TODO, payload: todos }
 }
--- src/reducers.js
+++ src/reducers.js
@@ -1,16 +1,17 @@
 import { combineReducers } from "redux";
+import { ADD_TO_DO, REVERT_STATUS, DELETE_TO_DO, INIT_TODO } from "./actions";
 
 const todos = (state = [], action) => {
     switch (action.type) {
-        case "ADD_TO_DO":
+        case ADD_TO_DO:
             return [...state, action.payload];
-        case "REVERT_STATUS":
+        case REVERT_STATUS:
             return state.map(todo =>
                 todo.id === action.payload ? { ...todo, done: !todo.done }
                     : todo);
-        case "DELETE_TO_DO":
+        case DELETE_TO_DO:
             return state.filter(todo => todo.id !== action.payload);
-        case "INIT_TODO":
+        case INIT_TODO:
             return action.payload;
         default:
             return state;
