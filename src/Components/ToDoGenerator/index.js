c233ea3ed1954c7e67fe11b3487235e239e2fd7c
John Emmanuel Bacalla <john.emmanuel.bacalla@oocl.com>
2020-10-28 15:27:40 +0800

    [TodoList][Bryan-John] feature: Post new todo in api

--- src/Components/ToDoGenerator/index.js
+++ src/Components/ToDoGenerator/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import { v4 as uuidv4 } from 'uuid';
 import './index.css'
+import { addTodo } from '../../apis/todos';
 
 class ToDoGenerator extends Component {
 
@@ -11,18 +11,17 @@ class ToDoGenerator extends Component {
             alert("To-Do item must be filled out");
             return false;
         }
-        const id = uuidv4();
-        const todo = { text, done: false, id }
-        this.props.addToDo(todo);
-        
-        event.target.toDoText.value = "";
+        addTodo(text).then(response => {
+            this.props.addToDo(response.data);
+            event.target.toDoText.value = "";
+        });
     }
 
     render() {
         return (
             <form onSubmit={this.onSubmit}>
-                <input type="text" name="toDoText" id="toDoText" placeholder="Input new to-do here..."/>
-                <input type="submit" value="Add"/>
+                <input type="text" name="toDoText" id="toDoText" placeholder="Input new to-do here..." />
+                <input type="submit" value="Add" />
             </form>
         );
     }
