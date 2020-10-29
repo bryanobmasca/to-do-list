30d0c7ac2afd4822b4367202c806397ebd5fee6f
John Emmanuel Bacalla <john.emmanuel.bacalla@oocl.com>
2020-10-29 09:53:48 +0800

    [TodoList][Bryan-John] refactor: Remove unnecessary braces

--- src/apis/todos.js
+++ src/apis/todos.js
@@ -1,6 +1,6 @@
 import api from './api'
 
-export const getTodos = () => (api.get('/api/todos'))
-export const addTodo = (text) => (api.post('/api/todos', { text }))
-export const deleteTodo = (id) => (api.delete(`/api/todos/${id}`))
-export const toggleTodo = ({ id, done }) => (api.put(`/api/todos/${id}`, { done: !done }))
+export const getTodos = () => api.get('/api/todos')
+export const addTodo = (text) => api.post('/api/todos', { text })
+export const deleteTodo = (id) => api.delete(`/api/todos/${id}`)
+export const toggleTodo = ({ id, done }) => api.put(`/api/todos/${id}`, { done: !done })
