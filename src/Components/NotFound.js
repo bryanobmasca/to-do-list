2aa6ff4fec783964b3cab3ab294870b1956879e1
bryanobmasca <bryanobmasca@gmail.com>
2020-10-29 09:14:06 +0800

    [TodoList][Bryan-John] refactor: Change the design of NotFound

--- src/Components/NotFound.js
+++ src/Components/NotFound.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
+import { Result } from 'antd';
 
 class NotFound extends Component {
 
     render() {
         return (
-            <div>NOT FOUND</div>
+            <Result
+                status="404"
+                title="404"
+                subTitle="Sorry, the page you visited does not exist."
+            />
         );
     }
 }
